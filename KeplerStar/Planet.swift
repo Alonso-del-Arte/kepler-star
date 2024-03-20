e46bf8dd9d330b56c58299bdf9f9a548db1b689b
Alonso del Arte <alonso.delarte@gmail.com>
2024-03-20 17:02:54 -0400

    New enum

---
+++ KeplerStar/Planet.swift
@@ -0,0 +1,32 @@
+//
+//  Planet.swift
+//  KeplerStar
+//
+//  Created by Alonso del Arte on 3/20/24.
+//
+
+import Foundation
+
+enum Planet {
+    
+    case sun
+    
+    case moon
+    
+    case mercury
+    
+    case venus
+    
+    case mars
+    
+    case jupiter
+    
+    case saturn
+    
+    case uranus
+    
+    case neptune
+    
+    case pluto
+    
+}
---
+++ KeplerStar/StarSign.swift
@@ -0,0 +1,37 @@
+//
+//  StarSign.swift
+//  KeplerStar
+//
+//  Created by Alonso del Arte on 3/20/24.
+//
+
+import Foundation
+import SwiftUI
+
+enum StarSign {
+    
+    case aries
+    
+    case taurus
+    
+    case gemini
+    
+    case cancer
+    
+    case leo
+    
+    case virgo
+    
+    case libra
+    
+    case scorpio
+    
+    case saggitarius
+    
+    case capricorn
+    
+    case aquarius
+    
+    case pisces
+    
+}
