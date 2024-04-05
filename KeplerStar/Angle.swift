43ce90aa2fd9fbafb4c2b5dd71968806f74aa282
Alonso del Arte <alonso.delarte@gmail.com>
2024-04-05 17:33:59 -0400

    New class, new test class

---
+++ KeplerStar/Angle.swift
@@ -0,0 +1,28 @@
+//
+//  Angle.swift
+//  KeplerStar
+//
+//  Created by Alonso del Arte on 4/5/24.
+//
+
+import Foundation
+
+struct Angle {
+    
+    func getDegrees() -> Int16 {
+        -1
+    }
+    
+    func getMinutes() -> UInt8 {
+        61
+    }
+    
+    func getSeconds() -> UInt8 {
+        61
+    }
+    
+    init(degrees: Int16, minutes: UInt8 = 0, seconds: UInt8 = 0) {
+        //
+    }
+    
+}
