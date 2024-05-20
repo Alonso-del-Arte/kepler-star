457087380a34759bd479485968f1cbcf71b82698
Alonso del Arte <alonso.delarte@gmail.com>
2024-05-20 16:39:46 -0400

    New enumerated type

---
+++ KeplerStar/Element.swift
@@ -0,0 +1,20 @@
+//
+//  Element.swift
+//  KeplerStar
+//
+//  Created by Alonso del Arte on 5/20/24.
+//
+
+import Foundation
+
+enum Element {
+    
+    case fire
+    
+    case earth
+    
+    case air
+    
+    case water
+    
+}
